16cff326d0766fe7f5d56d8860c6dfa4885e15a5
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-16 21:19:35 -0700

    initial commit

---
+++ src/main/java/com/fastfoodbackend/FastFoodBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.fastfoodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFoodBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastfoodbackend/FastFoodBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
